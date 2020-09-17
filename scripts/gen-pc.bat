5b62b0e31fa4bf6011c56b09833c621f19a333cc
thai.phi <thai.phi@indigames.net>
2020-09-17 13:59:50 +0700

    Update build scripts.

--- scripts/gen-pc.bat
+++ scripts/gen-pc.bat
@@ -8,36 +8,15 @@ SET BUILD_SHARED=0
 
 echo COMPILING ...
 SET PROJECT_DIR=%~dp0..
-
 SET BUILD_DIR=%PROJECT_DIR%\build\pc
-SET OUTPUT_DIR=%IGE_LIBS%\%LIB_NAME%
-SET OUTPUT_LIBS_DEBUG=%OUTPUT_DIR%\libs\pc\Debug
-SET OUTPUT_LIBS_RELEASE=%OUTPUT_DIR%\libs\pc
 
 SET CALL_DIR=%CD%
 
 echo Compiling %LIB_NAME% ...
 
-if not exist %OUTPUT_DIR% (
-    mkdir %OUTPUT_DIR%
-)
-
 if not exist %BUILD_DIR% (
     mkdir %BUILD_DIR%
 )
-
-echo Cleaning up...
-    if [%BUILD_DEBUG%]==[1] (
-        if exist %OUTPUT_LIBS_DEBUG% (
-            rmdir /s /q %OUTPUT_LIBS_DEBUG%
-        )
-        mkdir %OUTPUT_LIBS_DEBUG%
-    )
-
-    if exist %OUTPUT_LIBS_RELEASE% (
-        rmdir /s /q %OUTPUT_LIBS_RELEASE%
-    )
-    mkdir %OUTPUT_LIBS_RELEASE%
 
 cd %PROJECT_DIR%
 if [%BUILD_X86%]==[1] (
