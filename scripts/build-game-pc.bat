b40017e716ccafd0cd25335b0c3fbcdd724b7f17
thai.phi <thai.phi@indigames.net>
2020-10-01 14:03:35 +0700

    Minor: update executable files.

--- scripts/build-game-pc.bat
+++ scripts/build-game-pc.bat
@@ -1,38 +1,56 @@
 @echo off
 
-set CURR_DIR=%CD%
-set PROJECT_DIR=%~dp0..
-set ROM=%PROJECT_DIR%\release\ROM.zip
+SET LIB_NAME=igeGame
 
-if not exist %ROM% (
-    call %~dp0\build-rom.bat
+SET BUILD_DEBUG=0
+
+echo COMPILING ...
+SET PROJECT_DIR=%~dp0..\libs\igeGame
+SET BUILD_DIR=%~dp0..\build\game-pc
+SET OUTPUT_DIR=%~dp0..\app
+
+SET CALL_DIR=%CD%
+
+echo Compiling %LIB_NAME% ...
+
+if not exist %OUTPUT_DIR% (
+    mkdir %OUTPUT_DIR%
 )
 
-SET GAME_BUILDER=%APPDATA%\indigames\igeGameBuilder
+if not exist %BUILD_DIR% (
+    mkdir %BUILD_DIR%
+)
 
-C:
-if not exist "%GAME_BUILDER%" (
-    if not exist "%APPDATA%\indigames" (
-        md "%APPDATA%\indigames"
+cd %PROJECT_DIR%
+
+cd %PROJECT_DIR%
+echo Compiling x64...
+    if not exist %BUILD_DIR%\x64 (
+        mkdir %BUILD_DIR%\x64
     )
-    cd "%APPDATA%\indigames"
-    git clone https://github.com/indigames/igeGameBuilder --recursive
-)
+    echo Generating x64 CMAKE project ...
+    cd %BUILD_DIR%\x64
+    cmake %PROJECT_DIR% -A x64 -DAPP_STYLE=STATIC
+    if %ERRORLEVEL% NEQ 0 goto ERROR
 
-cd %GAME_BUILDER%
+    if [%BUILD_DEBUG%]==[1] (
+        echo Compiling x64 - Debug...
+        cmake --build . --config Debug -- -m
+        if %ERRORLEVEL% NEQ 0 goto ERROR
+        xcopy /q /s /y Debug\*.exe %OUTPUT_DIR%
+    ) else (
+        echo Compiling x64 - Release...
+        cmake --build . --config Release -- -m
+        if %ERRORLEVEL% NEQ 0 goto ERROR
+        xcopy /q /s /y Release\*.exe %OUTPUT_DIR%
+    )
+echo Compiling x64 DONE
 
-git checkout master
-git pull
-git submodule update --init --remote
+goto ALL_DONE
 
-xcopy /q /y %ROM% %GAME_BUILDER%
+:ERROR
+    echo ERROR OCCURED DURING COMPILING!
 
-call scripts\build-pc.bat
-
-if not exist %PROJECT_DIR%\release (
-    mkdir %PROJECT_DIR%\release
-)
-xcopy /q /y %GAME_BUILDER%\release\*.zip %PROJECT_DIR%\release
-
-%CURR_DIR:~0,2%
-cd %CURR_DIR%
+:ALL_DONE
+    cd %CALL_DIR%
+    echo COMPILING DONE!
