07a858826cdf1c95ce158faa21f920b3d7cf0e15
thai.phi <thai.phi@indigames.net>
2021-05-10 14:39:32 +0700

    Update: fix build ROM issue with existing "release" folder.

--- app/tools/build-rom.bat
+++ app/tools/build-rom.bat
@@ -14,7 +14,8 @@ if [%PROJECT_DIR%]==[] (
 
 SET TOOLS_DIR=%~dp0\tools
 
-SET ROM_PATH=!PROJECT_DIR!\.tmp\rom
+SET PROJECT_TMP=!PROJECT_DIR!\.tmp
+SET ROM_PATH=!PROJECT_TMP!\rom
 SET DST_PATH=!PROJECT_DIR!\release
 
 cd !PROJECT_DIR!
@@ -23,40 +24,56 @@ echo Updating igeCore ...
 python -m pip install igeCore --upgrade --user
 if %ERRORLEVEL% NEQ 0 goto ERROR
 
+echo Creating temp project...
+if exist !PROJECT_TMP! (
+    rmdir /s /q !PROJECT_TMP!
+)
+mkdir !PROJECT_TMP!
+
+if exist "!DST_PATH!" (
+    rmdir /s/ q "!DST_PATH!"
+)
+mkdir !DST_PATH!
+
+for /d %%i in (!PROJECT_DIR!\*) do (
+    ECHO %%i| FINDSTR /C:"!PROJECT_TMP!" > nul && (
+        echo Excluded: %%i
+    ) || (
+        xcopy /q /s /y %%i\* !PROJECT_TMP!\%%~nxi\
+    )
+)
+xcopy /q /y !PROJECT_DIR!\* !PROJECT_TMP!\
+
+if exist "!PROJECT_TMP!\config" (
+    rmdir /s /q "!PROJECT_TMP!\config"
+)
+
+mkdir !ROM_PATH!
+
 echo Building ROMs ...
-if exist "!PROJECT_DIR!\.tmp" (
-    rmdir /s /q  "!PROJECT_DIR!\.tmp"
-)
-if exist "!PROJECT_DIR!\release" (
-    rmdir /s /q  "!PROJECT_DIR!\release"
-)
-mkdir %ROM_PATH%
 
-cd !PROJECT_DIR!
-python %~dp0\build.py -p %PLATFORM_PC% -i . -o %ROM_PATH%\pc -c False
+cd !PROJECT_TMP!
+python %~dp0\build.py -p %PLATFORM_PC% -i . -o !ROM_PATH!\pc -c False
 if %ERRORLEVEL% NEQ 0 goto ERROR
-if exist "%ROM_PATH%\pc\config" (
-    rmdir /s /q  "%ROM_PATH%\pc\config"
+if exist "!ROM_PATH!\pc\config" (
+    rmdir /s /q  "!ROM_PATH!\pc\config"
 )
 
-python %~dp0\build.py -p %PLATFORM_ANDROID% -i . -o %ROM_PATH%\android -c False
+python %~dp0\build.py -p %PLATFORM_ANDROID% -i . -o !ROM_PATH!\android -c False
 if %ERRORLEVEL% NEQ 0 goto ERROR
-if exist "%ROM_PATH%\android\config" (
-    rmdir /s /q  "%ROM_PATH%\android\config"
+if exist "!ROM_PATH!\android\config" (
+    rmdir /s /q  "!ROM_PATH!\android\config"
 )
 
 echo Compressing the result...
 cd !PROJECT_DIR!
-if not exist %DST_PATH% (
-    mkdir %DST_PATH%
+if exist !DST_PATH!\ROM.zip (
+    del /f /q !DST_PATH!\ROM.zip
 )
-if exist %DST_PATH%\ROM.zip (
-    del /f /q %DST_PATH%\ROM.zip
-)
-%TOOLS_DIR%\7z.exe a %DST_PATH%\ROM.zip %ROM_PATH%
+%TOOLS_DIR%\7z.exe a !DST_PATH!\ROM.zip !ROM_PATH!
 if exist "!PROJECT_DIR!\config" (
-    %TOOLS_DIR%\7z.exe a %DST_PATH%\ROM.zip !PROJECT_DIR!\config
-    %TOOLS_DIR%\7z.exe a %DST_PATH%\ROM.zip !PROJECT_DIR!\config\app.yaml
+    %TOOLS_DIR%\7z.exe a !DST_PATH!\ROM.zip !PROJECT_DIR!\config
+    %TOOLS_DIR%\7z.exe a !DST_PATH!\ROM.zip !PROJECT_DIR!\config\app.yaml
 )
 if %ERRORLEVEL% NEQ 0 goto ERROR
 
@@ -68,11 +85,8 @@ goto ALL_DONE
 
 :ALL_DONE
     echo Cleaning up...
-    if exist "%ROM_PATH%" (
-        rmdir /s /q %ROM_PATH%
-    )
-    if exist "%PROJECT_DIR%\.tmp" (
-        rmdir /s /q %PROJECT_DIR%\.tmp
+    if exist "!PROJECT_TMP!" (
+        rmdir /s /q !PROJECT_TMP!
     )
     cd %CURR_DIR%
     echo ALL DONE!
