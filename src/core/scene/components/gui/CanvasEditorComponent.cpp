1e9a475bb3a42236ce4e8b6557613974ee6cb703
Viet Nguyen <viet.nguyen@indigames.net>
2021-09-29 11:03:29 +0700

    [Update] add canvas scaler mode

--- src/core/scene/components/gui/CanvasEditorComponent.cpp
+++ src/core/scene/components/gui/CanvasEditorComponent.cpp
@@ -42,5 +42,27 @@ void CanvasEditorComponent::drawCanvas()
         auto canvas = std::dynamic_pointer_cast<Canvas>(getComponent<CompoundComponent>()->getComponents()[0]);
         canvas->setTargetCanvasSize({ val[0], val[1] });
     });
+
+    auto mode = canvas->getScreenMatchMode();
+    auto modeCombo = m_canvasGroup->createWidget<ComboBox>("Screen Match Mode", (int)mode);
+    modeCombo->getOnDataChangedEvent().addListener([this](auto val) {
+        if (val != -1) {
+            auto canvas = std::dynamic_pointer_cast<Canvas>(getComponent<CompoundComponent>()->getComponents()[0]);
+            canvas->setScreenMatchMode((int)val);
+            setDirty();
+        }
+        });
+    modeCombo->setEndOfLine(false);
+    modeCombo->addChoice((int)ScreenMatchMode::MatchWidthOrHeight, "Match Width Or Height");
+    modeCombo->addChoice((int)ScreenMatchMode::Expand, "Expand");
+    modeCombo->addChoice((int)ScreenMatchMode::Shrink, "Shrink");
+    modeCombo->setEndOfLine(true);
+    if (mode == ScreenMatchMode::MatchWidthOrHeight) {
+        std::array matchValue = { canvas->getMatchWidthOrHeight() };
+        m_canvasGroup->createWidget<Drag<float>>("Size", ImGuiDataType_Float, matchValue, 0.001f, 0, 1)->getOnDataChangedEvent().addListener([this](auto& val) {
+            auto canvas = std::dynamic_pointer_cast<Canvas>(getComponent<CompoundComponent>()->getComponents()[0]);
+            canvas->setMatchWidthOrHeight(val[0]);
+            });
+    }
 }
 NS_IGE_END
