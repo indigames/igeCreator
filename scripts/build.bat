b7ae30e9082a18d356393a6e38ee3d04eecc2c7a
thai.phi <thai.phi@indigames.net>
2021-10-01 13:30:32 +0700

    Update: allow build machine to use different conan repo.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,14 +1,15 @@
 @echo off
+setlocal enabledelayedexpansion
 
 set CALL_DIR=%CD%
 set PROJECT_DIR=%~dp0..
 
-set CONAN_REVISIONS_ENABLED=1
-
-conan config set storage.download_cache="D:\DevTools\.conan_cache"
-set CONAN_USER_HOME=D:\DevTools\.conan_igeCreator
-
-conan remote add ige-center http://10.1.0.222:8081/artifactory/api/conan/conan --force
+if [%CONAN_USER_HOME%]==[] (
+    set CONAN_REVISIONS_ENABLED=1
+    set CONAN_USER_HOME=D:\DevTools\.conan_igeCreator
+    conan config set storage.download_cache="D:\DevTools\.conan_cache"
+    conan remote add ige-center http://10.1.0.222:8081/artifactory/api/conan/conan --force
+)
 
 cd %PROJECT_DIR%
 python %PROJECT_DIR%/build.py
