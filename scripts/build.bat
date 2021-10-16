f24c5a760cf3bbe0d5d3c13992eb7297e147efe6
thai.phi <thai.phi@indigames.net>
2021-10-16 16:30:43 +0700

    Update: add document generation.

--- scripts/build.bat
+++ scripts/build.bat
@@ -14,6 +14,13 @@ if [%CONAN_USER_HOME%]==[] (
     conan remote add ige-center http://10.1.0.222:8081/artifactory/api/conan/conan --force
 )
 
+python -m pip install pdoc3 --user --upgrade
+python -m pip install -i https://test.pypi.org/simple/ igeCore igeVmath igeScene --user --upgrade
+
+python -m pdoc --config show_inherited_members=True --html igeVmath -o %PROJECT_DIR%\app\doc --skip-errors --force
+python -m pdoc --config show_inherited_members=True --html igeCore -o %PROJECT_DIR%\app\doc --skip-errors --force
+python -m pdoc --config show_inherited_members=True --html igeScene -o %PROJECT_DIR%\app\doc --skip-errors --force
+
 cd %PROJECT_DIR%
 python %PROJECT_DIR%/build.py
 
