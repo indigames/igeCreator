e8a4b4d1088d4563642b354821ce932a259f35dd
thai.phi <thai.phi@indigames.net>
2021-10-20 10:06:53 +0700

    Update: always rebuild igeScene for updated document.

--- scripts/build.bat
+++ scripts/build.bat
@@ -12,14 +12,17 @@ if [%CONAN_USER_HOME%]==[] (
     conan remote disable conancenter
     conan remote disable conan-center
     conan remote add ige-center http://10.1.0.222:8081/artifactory/api/conan/conan --force
+) else (
+    REM This should be build machine environment
+    python -m pip install pdoc3 --user --upgrade
+    python -m pip install -i https://test.pypi.org/simple/ igeCore igeVmath --user --upgrade
+    
+    python -m pip install %PROJECT_DIR%\libs\igeScene --user
+    
+    python -m pdoc --config show_inherited_members=True --html igeVmath -o %PROJECT_DIR%\app\doc --skip-errors --force
+    python -m pdoc --config show_inherited_members=True --html igeCore -o %PROJECT_DIR%\app\doc --skip-errors --force
+    python -m pdoc --config show_inherited_members=True --html igeScene -o %PROJECT_DIR%\app\doc --skip-errors --force
 )
-
-python -m pip install pdoc3 --user --upgrade
-python -m pip install -i https://test.pypi.org/simple/ igeCore igeVmath igeScene --user --upgrade
-
-python -m pdoc --config show_inherited_members=True --html igeVmath -o %PROJECT_DIR%\app\doc --skip-errors --force
-python -m pdoc --config show_inherited_members=True --html igeCore -o %PROJECT_DIR%\app\doc --skip-errors --force
-python -m pdoc --config show_inherited_members=True --html igeScene -o %PROJECT_DIR%\app\doc --skip-errors --force
 
 cd %PROJECT_DIR%
 python %PROJECT_DIR%/build.py
