3d26d4267af3a0d7ee799c9f9e61a025f775a9ab
thai.phi <thai.phi@indigames.net>
2021-12-01 22:43:51 +0700

    Bug: fix script component remove wrong item.

--- src/core/scene/TargetObject.cpp
+++ src/core/scene/TargetObject.cpp
@@ -129,7 +129,19 @@ namespace ige::scene
             for (auto& object : m_objects)
             {
                 if (!object.expired())
-                    object.lock()->removeComponent((*found)->getName());
+                    if((*found)->canMultiEdit()) {
+                        // just remove component by name
+                        object.lock()->removeComponent((*found)->getName());
+                    }
+                    else {
+                        // remove exact component by id
+                        auto& compoundComponent = std::dynamic_pointer_cast<CompoundComponent>((*found));
+                        for (auto& comp : compoundComponent->getComponents()) {
+                            object.lock()->removeComponent(comp->getInstanceId());
+                            comp = nullptr;
+                        }
+                        compoundComponent = nullptr;
+                    }
             }
             m_components.erase(found);
             return true;
