5dfbf768a30ac0ea5169ae62fc675c769c0c83ee
thai.phi <thai.phi@indigames.net>
2022-07-06 16:06:21 +0700

    Update: support emscripten.

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 import os
 import shutil
 import platform
-from conanfile import IgeConan
+from pathlib import Path
+import multiprocessing
+
+cpu_count = multiprocessing.cpu_count()
+project_dir = os.path.abspath(os.path.dirname(__file__))
 
 def setEnv(name, value):
     if platform.system() == 'Windows':
@@ -19,24 +23,54 @@ def safeRemove(rm_path):
     except:
         pass
 
-def build(platform, arch):
-    safeRemove('build')
-    try:
-        os.mkdir('build')
-    except:
-        pass
-    os.chdir('build')
-    ret_code = os.system(f'conan install --update .. --profile ../cmake/profiles/{platform}_{arch}')
-    if ret_code != 0:
+def _generateCMakeProject(platform, arch):
+    cmake_cmd = f'cmake {project_dir} -DCMAKE_BUILD_TYPE=Release '
+    if platform == "windows":
+        if arch == "x86":
+            cmake_cmd += f' -A Win32'
+        else:
+            cmake_cmd += f' -A X64'
+    elif platform == "android":
+        toolchain = Path(os.environ.get("ANDROID_NDK_ROOT")).absolute().as_posix() + '/build/cmake/android.toolchain.cmake'
+        if arch == "armv7":
+            cmake_cmd += f' -G Ninja -DCMAKE_TOOLCHAIN_FILE={toolchain} -DANDROID_ABI=armeabi-v7a -DANDROID_PLATFORM=android-21'
+        else:
+            cmake_cmd += f' -G Ninja -DCMAKE_TOOLCHAIN_FILE={toolchain} -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-21'
+    elif platform == "ios":
+        toolchain = Path(self.source_folder).absolute().as_posix() + '/cmake/ios.toolchain.cmake'
+        cmake_cmd += f' -G Xcode -DCMAKE_TOOLCHAIN_FILE={toolchain} -DIOS_DEPLOYMENT_TARGET=11.0 -DPLATFORM=OS64'
+    elif platform == "macos":
+        cmake_cmd += f' -G Xcode -DOSX=1'
+    elif platform == "emscripten":
+        toolchain = Path(os.environ.get("EMSCRIPTEN_ROOT_PATH")).absolute().as_posix() + '/cmake/Modules/Platform/Emscripten.cmake'
+        cmake_cmd += f' -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE={toolchain}'
+    else:
+        print(f'Configuration not supported: platform = {platform}, arch = {arch}')
         exit(1)
 
-    os.chdir('..')
-    ret_code = os.system('conan build . --build-folder build')
-    if ret_code != 0:
-        exit(1)
+    os.system(cmake_cmd)
+
+def _buildCMakeProject(platform):
+    platform_flags = f"-- -m:{cpu_count} -p:BuildInParallel=true /p:CL_MPcount={cpu_count}" if platform == "windows" else ""
+    os.system(f'cmake --build . --config Release --target package --parallel {cpu_count} {platform_flags}')
+
+def build(platform, arch):
+    os.chdir(project_dir)
+    build_dir = Path(os.path.join('build', platform, arch)).as_posix()
+    safeRemove(build_dir)
+    os.makedirs(build_dir)
+    os.chdir(build_dir)
+    _generateCMakeProject(platform, arch)
+    _buildCMakeProject(platform)
+    os.chdir(project_dir)
+    release_dir = Path(os.path.join(project_dir, 'release')).as_posix()
+    safeRemove(release_dir)
+    os.makedirs(release_dir)
+    for fname in os.listdir(build_dir):
+        if fname.endswith('.exe') or fname.endswith('.zip'):
+            shutil.move(os.path.join(build_dir, fname), release_dir)
 
 def main():
-    setEnv('CONAN_REVISIONS_ENABLED', '1')
     if platform.system() == 'Windows':
         build('windows', 'x86_64')
     elif platform.system() == 'Darwin':
--- scripts/build.bat
+++ scripts/build.bat
@@ -4,15 +4,9 @@ setlocal enabledelayedexpansion
 set CALL_DIR=%CD%
 set PROJECT_DIR=%~dp0..
 
-if [%CONAN_USER_HOME%]==[] (
-    set CONAN_REVISIONS_ENABLED=1
-    set CONAN_USER_HOME=D:\DevTools\.conan_igeCreator
-    conan config set storage.download_cache="%APPDATA%\indigames\.conan_cache"
-    conan remote add ige-center http://10.1.0.222:8081/artifactory/api/conan/conan --force
-    conan remote disable "*"
-    conan remote enable ige-center
+if [%WORKSPACE%]==[] (
+    echo Building on local machine...
 ) else (
-    REM This should be build machine environment
     python -m pip install pdoc3 --user --upgrade
     python -m pip install -i https://test.pypi.org/simple/ igeCore igeVmath igeSdk --user --upgrade
     
